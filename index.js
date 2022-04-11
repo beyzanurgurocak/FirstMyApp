78350d230684b948987c00c79d9cde835d76c3f3
Beyzanur Gürocak <85355977+beyzanurgurocak@users.noreply.github.com>
2022-04-12 00:50:20 +0300

    jquery added project

---
+++ index.js
@@ -0,0 +1,22 @@
+// EVENTS
+// 1-Click
+let elemanClick=$('#loginBtn');
+elemanClick.click(DomClick)
+let eleman=$('#headerLogin');
+
+function DomClick(){
+ $(eleman).toggle()
+}
+
+
+
+
+// 2-Hover
+// let elemanHover=$('#loginBtn');
+// elemanHover.hover(DomHover)
+
+// function DomHover(){
+//     alert("Dom Hover")
+// }
+
+
